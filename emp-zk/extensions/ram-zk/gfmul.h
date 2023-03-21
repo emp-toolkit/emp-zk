803e1ac2b74917fee41c79ad707c22ee2bc4bae6
Xiao Wang <wangxiao1254@gmail.com>
2023-03-21 13:53:43 -0500

    update f2k

--- emp-zk/extensions/ram-zk/gfmul.h
+++ emp-zk/extensions/ram-zk/gfmul.h
@@ -24,66 +24,12 @@ inline void mul128_0(__m128i *a, __m128i *b, __m128i *res1, __m128i *res2) {
 	}
 }
 
-inline void reduction0(__m128i tmp3, __m128i tmp6, __m128i* res) {
-	/*__m128i tmp7, tmp8, tmp9, tmp10, tmp11, tmp12;
-	__m128i XMMMASK = _mm_setr_epi32(0xffffffff, 0x0, 0x0, 0x0);
-	tmp7 = _mm_srli_epi32(tmp6, 31);
-	tmp8 = _mm_srli_epi32(tmp6, 30);
-	tmp9 = _mm_srli_epi32(tmp6, 25);
-	tmp7 = _mm_xor_si128(tmp7, tmp8);
-	tmp7 = _mm_xor_si128(tmp7, tmp9);
-	tmp8 = _mm_shuffle_epi32(tmp7, 147);
-
-	tmp7 = _mm_and_si128(XMMMASK, tmp8);
-	tmp8 = _mm_andnot_si128(XMMMASK, tmp8);
-	tmp3 = _mm_xor_si128(tmp3, tmp8);
-	tmp6 = _mm_xor_si128(tmp6, tmp7);
-	tmp10 = _mm_slli_epi32(tmp6, 1);
-	tmp3 = _mm_xor_si128(tmp3, tmp10);
-	tmp11 = _mm_slli_epi32(tmp6, 2);
-	tmp3 = _mm_xor_si128(tmp3, tmp11);
-	tmp12 = _mm_slli_epi32(tmp6, 7);
-	tmp3 = _mm_xor_si128(tmp3, tmp12);
-
-	*res = _mm_xor_si128(tmp3, tmp6);*/
-    __m128i tmp2, tmp4, tmp5, tmp7, tmp8, tmp9;
-    tmp7 = _mm_srli_epi32(tmp3, 31);
-    tmp8 = _mm_srli_epi32(tmp6, 31);
-    tmp3 = _mm_slli_epi32(tmp3, 1);
-    tmp6 = _mm_slli_epi32(tmp6, 1);
-
-    tmp9 = _mm_srli_si128(tmp7, 12);
-    tmp8 = _mm_slli_si128(tmp8, 4);
-    tmp7 = _mm_slli_si128(tmp7, 4);
-    tmp3 = _mm_or_si128(tmp3, tmp7);
-    tmp6 = _mm_or_si128(tmp6, tmp8);
-    tmp6 = _mm_or_si128(tmp6, tmp9);
-
-    tmp7 = _mm_slli_epi32(tmp3, 31);
-    tmp8 = _mm_slli_epi32(tmp3, 30);
-    tmp9 = _mm_slli_epi32(tmp3, 25);
-    tmp7 = _mm_xor_si128(tmp7, tmp8);
-    tmp7 = _mm_xor_si128(tmp7, tmp9);
-    tmp8 = _mm_srli_si128(tmp7, 4);
-    tmp7 = _mm_slli_si128(tmp7, 12);
-    tmp3 = _mm_xor_si128(tmp3, tmp7);
-
-    tmp2 = _mm_srli_epi32(tmp3, 1);
-    tmp4 = _mm_srli_epi32(tmp3, 2);
-    tmp5 = _mm_srli_epi32(tmp3, 7);
-    tmp2 = _mm_xor_si128(tmp2, tmp4);
-    tmp2 = _mm_xor_si128(tmp2, tmp5);
-    tmp2 = _mm_xor_si128(tmp2, tmp8);
-    tmp3 = _mm_xor_si128(tmp3, tmp2);
-    *res = _mm_xor_si128(tmp6, tmp3);
-}
-
 template<int N>
 inline void gfmul_1(__m128i *a, __m128i *b, __m128i *res) {
 	__m128i tmp3[N], tmp6[N];
 	mul128_0<N>(a, b, tmp3, tmp6);
 	for(int i = 0; i < N; ++i)
-		reduction0(tmp3[i], tmp6[i], res+i);
+		res[i] = reduce(tmp3[i], tmp6[i]);
 }
 
 /* batch 3 */
@@ -120,7 +66,7 @@ inline void gfmul3(__m128i &a1, __m128i &a2, __m128i &a3,
 	__m128i tmp3[3], tmp6[3];
 	mul128_3(a1, a2, a3, b1, b2, b3, tmp3, tmp6);
 	for(int i = 0; i < 3; ++i)
-		reduction0(tmp3[i], tmp6[i], res+i);
+		res[i] = reduce(tmp3[i], tmp6[i]);
 }
 
 
@@ -162,7 +108,7 @@ inline void gfmul4(__m128i &a1, __m128i &a2, __m128i &a3, __m128i &a4,
 	__m128i tmp3[4], tmp6[4];
 	mul128_4(a1, a2, a3, a4, b1, b2, b3, b4, tmp3, tmp6);
 	for(int i = 0; i < 4; ++i)
-		reduction0(tmp3[i], tmp6[i], res+i);
+		res[i] = reduce(tmp3[i], tmp6[i]);
 }
 
 
@@ -216,7 +162,7 @@ inline void gfmul6(__m128i &a1, __m128i &a2, __m128i &a3,
 	__m128i tmp3[6], tmp6[6];
 	mul128_6(a1, a2, a3, a4, a5, a6, b1, b2, b3, b4, b5, b6, tmp3, tmp6);
 	for(int i = 0; i < 6; ++i)
-		reduction0(tmp3[i], tmp6[i], res+i);
+		res[i] = reduce(tmp3[i], tmp6[i]);
 }
 #endif
 
